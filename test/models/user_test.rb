e82e5192d8f034a3e7779b48841a99db1ece7f3d
Robson Lopes <roblucci100@gmail.com>
2022-11-28 21:27:41 -0300

    Adição de testes para o user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,8 +20,23 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "should not save without name" do
+    @user.name = ''
+    assert_not @user.save, "Saved the user without a name"
+  end
+
+  test "should save user with a name" do
+    assert @user.save, "User with a name was saved sucessfully"
+  end
+
   test 'user name should not have more than 80 characters' do
     @user.name = 'a' * 81
     assert_not @user.valid?
+  end
+
+  test 'should not accept more than one account with the same email' do
+    assert @user.save
+    new_user = User.create(email: 'johndoe@example.com')
+    assert_not new_user.save
   end
 end
